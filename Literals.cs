243316915a3c8a907f8657fec6cd67bb163f00a6
John alex <jhonalexjm@gmail.com>
2021-02-16 17:35:45 -0500

    Azure Function upload, download and resize photos -- used: cosmosdb and azure storage emulator

---
+++ Literals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotosAzure
+{
+    public static class Literals
+    {
+        public const string StorageConnectionString = nameof(StorageConnectionString);
+        public const string CosmosDBConnection = nameof(CosmosDBConnection);
+    }
+}
---
+++ Models/PhotoUploadModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace PhotosAzure.Models
+{
+    public class PhotoUploadModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("tags")]
+        public string[] Tags { get; set; }
+
+        [JsonProperty("photo")]
+        public string Photo { get; set; }
+    }
+}
